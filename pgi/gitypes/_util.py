c7dbbfff233f5b04cc1f99120a0f602daa8683a7
Christoph Reiter <christoph.reiter@gmx.at>
2012-07-11 17:15:07 +0200

    load c functions on demand

--- pgi/gitypes/_util.py
+++ pgi/gitypes/_util.py
@@ -4,42 +4,38 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-_dll_cache = {}
+from ctypes import CDLL
+
+
 _so_mapping = {
     "glib-2.0": "libglib-2.0.so.0",
     "gobject-2.0": "libgobject-2.0.so.0",
     "girepository-1.0": "libgirepository-1.0.so.1",
 }
 
-
-def load(name):
-    global _dll_cache, _so_mapping
-    if name not in _dll_cache:
-        from ctypes import CDLL
-        _dll_cache[name] = CDLL(_so_mapping[name])
-    return _dll_cache[name]
-
-count = 0
+load = lambda name: CDLL(_so_mapping[name])
 
 
-def _debug(f, name, base):
-    def _add(*args):
-        global count
-        count += 1
-        print count, base.__name__ + "." + name
-        return f(*args)
-    return _add
+class _CMethod(object):
+    def __init__(self, *args):
+        self.args = args
 
-
-def wrap_class(lib, base, ptr, prefix, methods):
-    for name, ret, args in methods:
+    def __get__(self, instance, owner):
+        lib, name, prefix, ret, args, wrap = self.args
         func = getattr(lib, prefix + name)
         func.argtypes = args
         func.restype = ret
 
+        if wrap:
+            setattr(owner, name, (lambda f: lambda *x: f(*x))(func))
+            return getattr(instance, name)
+        setattr(owner, name, func)
+        return func
+
+
+def wrap_class(lib, base, ptr, prefix, methods):
+    for name, ret, args in methods:
         if args and args[0] == ptr:
-            add_self = lambda f: lambda *args: f(*args)
-            setattr(ptr, name, add_self(func))
-            #setattr(ptr, name, _debug(func, name, base))
+            setattr(ptr, name, _CMethod(lib, name, prefix, ret, args, True))
         else:
-            setattr(base, name, func)
+            setattr(base, name, _CMethod(lib, name, prefix, ret, args, False))
