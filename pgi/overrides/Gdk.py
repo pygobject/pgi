319a3c076b698e3357c21d51b538274d332015b2
Christoph Reiter <reiter.christoph@gmail.com>
2015-10-29 14:53:27 +0100

    support the new Gdk.Rectangle in newer gtk+

--- pgi/overrides/Gdk.py
+++ pgi/overrides/Gdk.py
@@ -126,10 +126,22 @@ if Gdk._version == '2.0':
     Rectangle = override(Rectangle)
     __all__.append('Rectangle')
 else:
-    from pgi.repository import cairo as _cairo
-    Rectangle = _cairo.RectangleInt
+    # Newer GTK+/gobject-introspection (3.17.x) include GdkRectangle in the
+    # typelib. See https://bugzilla.gnome.org/show_bug.cgi?id=748832 and
+    # https://bugzilla.gnome.org/show_bug.cgi?id=748833
+    if not hasattr(Gdk, 'Rectangle'):
+        from gi.repository import cairo as _cairo
+        Rectangle = _cairo.RectangleInt
 
-    __all__.append('Rectangle')
+        __all__.append('Rectangle')
+    else:
+        # https://bugzilla.gnome.org/show_bug.cgi?id=756364
+        # These methods used to be functions, keep aliases for backwards compat
+        rectangle_intersect = Gdk.Rectangle.intersect
+        rectangle_union = Gdk.Rectangle.union
+
+        __all__.append('rectangle_intersect')
+        __all__.append('rectangle_union')
 
 if Gdk._version == '2.0':
     class Drawable(Gdk.Drawable):
