93cd183033252ed2e5447412e36c7ec8d07b6af4
Christoph Reiter <reiter.christoph@gmail.com>
2016-02-11 18:31:32 +0100

    some docs for static bindings functions

--- pgi/properties.py
+++ pgi/properties.py
@@ -279,26 +279,31 @@ class _PropsDescriptor(object):
         return attr
 
 
-def list_properties(cls):
-    """list_properties(cls: GObject.Object or GObject.GInterface or GObejct.GType) -> [GObject.ParamSpec]
+def list_properties(type):
+    """
+    :param type: a Python GObject instance or type that the signal is associated with
+    :type type: :obj:`GObject.Object`
+
+    :returns: a list of :obj:`GObject.ParamSpec`
+    :rtype: [:obj:`GObject.ParamSpec`]
 
     Takes a GObject/GInterface subclass or a GType and returns a list of
-    GParamSpecs of all properties.
+    GParamSpecs for all properties of `type`.
     """
 
-    if isinstance(cls, PGType):
-        cls = cls.pytype
+    if isinstance(type, PGType):
+        type = type.pytype
 
     from pgi.obj import Object, InterfaceBase
 
-    if not issubclass(cls, (Object, InterfaceBase)):
+    if not issubclass(type, (Object, InterfaceBase)):
         raise TypeError("Must be a subclass of %s or %s" %
                         (Object.__name__, InterfaceBase.__name__))
 
     gparams = []
-    for key in dir(cls.props):
+    for key in dir(type.props):
         if not key.startswith("_"):
-            gparams.append(getattr(cls.props, key))
+            gparams.append(getattr(type.props, key))
     return gparams
 
 
--- pgi/static.py
+++ pgi/static.py
@@ -51,12 +51,33 @@ def _gvalue_get(self):
     return type(self).__mro__[1].get_boxed(self)
 
 
-def type_register(*args, **kwargs):
+def type_register(class_):
+    """
+    :param class_: a Python class that is a descendant of :obj:`GObject.Object`
+
+    The GObject.type_register() function registers the specified Python class
+    as a GObject type. `class_` must be a descendant of GObject.Object.
+    The function generates a name for the new type.
+    """
+
     raise NotImplementedError
 
 
-def new(gtype_or_similar):
-    return GType(gtype_or_similar).pytype()
+def new(gtype_or_similar, **kwargs):
+    """
+    :param type: a Python GObject type
+    :type type: :obj:`GObject.Object`
+
+    :param kwargs: set of property-value pairs
+
+    :returns: a new object of the specified `type`
+
+    The Gobject.new() function returns a new object of the specified type.
+    `type` must specify a type that is a descendant of gobject.GObject.
+    GObject properties can set via keyword arguments.
+    """
+
+    return GType(gtype_or_similar).pytype(**kwargs)
 
 
 def _min_value(ctypes_type):
@@ -97,11 +118,62 @@ class Pid(object):
         raise NotImplementedError
 
 
-def add_emission_hook(*args, **kwargs):
+def add_emission_hook(type, name, callback, *user_data):
+    """
+    :param type: a Python GObject instance or type
+    :type type: :obj:`GObject.Object`
+
+    :param name: a signal name
+    :type name: :obj:`str`
+
+    :param callback: a function
+
+    :param user_data: zero or more extra arguments that will be passed to callback
+
+    The add_emission_hook() function adds an emission hook for the signal
+    specified by name, which will get called for any emission of that signal,
+    independent of the instance. This is possible only for signals which don't
+    have the :obj:`GObject.SignalFlags.NO_HOOKS` flag set.
+    """
+
     raise NotImplementedError
 
 
-def signal_new(*args, **kwargs):
+def signal_new(signal_name, type, flags, return_type, param_types):
+    """
+    :param signal_name: the name of the signal
+    :type signal_name: :obj:`str`
+
+    :param type: a Python GObject instance or type that the signal is associated with
+    :type type: :obj:`GObject.Object`
+
+    :param flags: the signal flags
+    :type flags: :obj:`GObject.SignalFlags`
+
+    :param return_type: the return type of the signal handler
+    :type return_type: :obj:`type`
+
+    :param param_types: the parameter types passed to the signal handler
+    :type param_types: [:obj:`type`]
+
+    :returns: a unique integer signal ID
+    :rtype: :obj:`int`
+
+    The :obj:`GObject.signal_new`\() function registers a signal with the
+    specified `signal_name` for the specified object `type`.
+
+    `return_type` is the type of the return value from a signal handler and may
+    be a gobject type, type ID or instance. The `param_types` parameter is a
+    list of additional types that are passed to the signal handler. Each
+    parameter type may be specified as a gobject type, type ID or instance.
+    For example, to add a signal to the :obj:`Gtk.Window` type called "my-signal"
+    that calls a handler with a :obj:`Gtk.Button` widget and an integer value and a
+    return value that is a boolean, use::
+
+          GObject.signal_new("my_signal", Gtk.Window, GObject.SignalFlags.RUN_LAST, GObject.TYPE_BOOLEAN, (Gtk.Button, GObject.TYPE_INT))
+
+    """
+
     raise NotImplementedError
 
 
